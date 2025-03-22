2d8bd0906fb877c502fda6615667363856e42c31
connorkuljis <conkuljis@gmail.com>
2025-03-22 16:16:45 +0800

    keymap: insert_timestamp

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -19,3 +19,17 @@ vim.keymap.set("n", "<leader>e", "<cmd>Explore <CR>")
 vim.api.nvim_create_user_command('EditConfig', function()
 	vim.cmd('edit ~/.config/nvim')
 end, {})
+
+function insert_timestamp()
+	-- Get the current timestamp
+	local timestamp = os.date("%Y-%m-%d %H:%M:%S")
+
+	-- Get the current buffer and cursor position
+	local buf = vim.api.nvim_get_current_buf()
+	local row, col = unpack(vim.api.nvim_win_get_cursor(0)) -- 0 refers to the current window
+
+	-- Insert the timestamp at the current cursor position
+	vim.api.nvim_buf_set_text(buf, row - 1, col, row - 1, col, { timestamp })
+end
+
+vim.api.nvim_set_keymap("n", "<leader>ts", "<cmd>lua insert_timestamp()<CR>", { noremap = true, silent = true })
